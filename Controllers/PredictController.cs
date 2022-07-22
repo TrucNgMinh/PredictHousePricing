be5800938c6fb352aefeccc0795f8c4ba78fb106
Trực Nguyễn <truc.bugcreator666@gmail.com>
2022-07-23 01:18:24 +0700

    update
    Update latest code

---
+++ Controllers/PredictController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.ML;
+using System;
+using static HousePricePrediction.PricePredictMLM;
+
+namespace HousePricePrediction.Controllers
+{
+    [ApiController]
+    [Produces("application/json")]
+    [Route("[controller]")]
+    public class PredictController : ControllerBase
+    {
+        private readonly PredictionEnginePool<ModelInput, ModelOutput> _predictionEnginePool;
+
+        public PredictController(PredictionEnginePool<ModelInput, ModelOutput> predictionEnginePool)
+        {
+            _predictionEnginePool = predictionEnginePool;
+        }
+
+        [Route("~/api/Predict/PriceHousePredict")]
+        [HttpPost]
+        public ActionResult<string> PriceHousePredict([FromBody] ModelInput input)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            ModelOutput prediction = _predictionEnginePool.Predict(input);
+
+            return Ok(Math.Round( prediction.Score, 2).ToString());
+        }
+    }
+}
---
+++ wwwroot/assets/pages/jquery.dashboard.init.js
@@ -0,0 +1,22 @@
+! function($) {
+    "use strict";
+
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function() {
+        var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            if (currentScrollPos == 0) {
+                document.getElementById("topnav").style.top = "0";
+            } else {
+                document.getElementById("topnav").style.top = "-90px";
+            }
+        } else {
+            document.getElementById("topnav").style.top = "-170px";
+        }
+        prevScrollpos = currentScrollPos;
+    }
+}(window.jQuery),
+
+function($) {
+    "use strict";
+}(window.jQuery);
